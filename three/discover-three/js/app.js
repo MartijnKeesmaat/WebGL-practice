bd8210190834664b28a348a0a4c8e0cbdf9dfb3a
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-04-10 15:02:39 +0200

    Choo update

--- three/discover-three/js/app.js
+++ three/discover-three/js/app.js
@@ -29,6 +29,7 @@ function init() {
 
 }
 
+
 function createCamera() {
 
 	camera = new THREE.PerspectiveCamera(
@@ -42,11 +43,11 @@ function createCamera() {
 
 }
 
+
 function createControls() {
-
 	controls = new THREE.OrbitControls(camera, container);
-
 }
+
 
 function createLights() {
 
@@ -63,13 +64,19 @@ function createLights() {
 
 }
 
+
 function createGeometries() {
 	const nose = new THREE.CylinderBufferGeometry(0.75, 0.75, 2, 12);
 	const cabin = new THREE.BoxBufferGeometry(2, 2.25, 1.5);
+	const chimney = new THREE.CylinderBufferGeometry(0.3, 0.1, 0.5);
+	const wheel = new THREE.CylinderBufferGeometry(0.4, 0.4, 1.75, 16);
+	wheel.rotateX(Math.PI / 2);
 
 	return {
 		nose,
-		cabin
+		cabin,
+		chimney,
+		wheel
 	};
 }
 
@@ -94,8 +101,8 @@ function createMaterials() {
 	};
 }
 
+const train = new THREE.Group();
 function createMeshes() {
-	const train = new THREE.Group();
 	scene.add(train);
 	const materials = createMaterials();
 	const geometries = createGeometries();
@@ -106,8 +113,35 @@ function createMeshes() {
 	nose.position.x = -1;
 
 	// Cabin
-	const cabin = new THREE.mesh(geometries.cabin, materials.body);
-	cabin.position.set(1.5, 0.4, 0); // shorthand for thingie.position.x y z
+	const cabin = new THREE.Mesh(geometries.cabin, materials.body);
+	cabin.position.set(1, 0.4, 0); // shorthand for thingie.position.x y z
+
+	const chimney = new THREE.Mesh(geometries.chimney, materials.detail);
+	chimney.position.set(-1.5, 0.9, 0);
+
+	const smallWheelRear = new THREE.Mesh(geometries.wheel, materials.detail);
+	smallWheelRear.position.set(0, -0.5, 0);
+
+	const smallWheelCenter = smallWheelRear.clone();
+	smallWheelCenter.position.x = -1;
+
+	const smallWheelFront = smallWheelRear.clone();
+	smallWheelCenter.position.x = -2;
+
+	const bigWheel = smallWheelRear.clone();
+	bigWheel.scale.set(2, 2, 1.25);
+	bigWheel.position.set(1.5, -0.1, 0);
+
+	train.add(
+		nose,
+		cabin,
+		chimney,
+		smallWheelRear,
+		smallWheelCenter,
+		smallWheelFront,
+		bigWheel
+	);
+	train.position.x = 5;
 }
 
 function createRenderer() {
@@ -131,7 +165,7 @@ function createRenderer() {
 function update() {
 
 	// Don't delete this function!
-
+	train.position.x -= 0.02;
 }
 
 // render, or 'draw a still image', of the scene
